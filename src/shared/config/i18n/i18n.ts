a9bbc928f5260d7ea24ac45f0a5c5a5ad809483f
SlavaTikhonov <tikhonov.viache@gmail.com>
2024-04-06 16:33:56 +0300

    changed i18n.ts again

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -4,18 +4,6 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import translationEN from '../../../../public/locales/en/translation.json';
-import translationRU from '../../../../public/locales/en/translation.json';
-
-const resources = {
-    en: {
-        translation: translationEN,
-    },
-    nl: {
-        translation: translationRU,
-    },
-};
-
 i18n
     .use(Backend)
     .use(LanguageDetector)
@@ -23,9 +11,12 @@ i18n
     .init({
         fallbackLng: 'en',
         debug: __IS_DEV__,
-        resources,
+
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
+        },
+        backend: {
+            loadPath: 'https://SlavaTikhonov.github.io/weather-site/public/locales/{{lng}}/{{ns}}.json',
         },
     });
 
